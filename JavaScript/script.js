083fab0ca0b0ec93bc4e891a8737630de191e62d
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2023-01-01 03:06:22 +0300

    bjhjk

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,7 @@ const title = document.querySelector('.kuluptitle');
 const zirveimg = document.querySelector('.zirveimg');
 const text1 = document.querySelector('.zirvep');
 const konusmacilarimiz = document.querySelector('.konusmacilarimizmain');
+const remaining = document.querySelector('.countdowndivmain')
 var typing=new Typed(text, {
     strings: ["","Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam tenetur error quibusdam architecto! Consequuntur in quia a ipsa nemo error soluta expedita provident hic labore accusantium nobis, impedit dolorum"],
     typeSpeed: 10,
@@ -55,9 +56,9 @@ window.smoothScroll = function (target) {
   scroll = function (c, a, b, i) {
       i++;
       if (i > 30) return;
-      c.scrollTop = a + (b - a) / 30 * i;
+      c.scrollTop = a + (b - a) /30 * i;
       setTimeout(function () {
-          scroll(c, a, b, i);
+          scroll(c, a, b-4, i);
       }, 2);
   }
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
@@ -93,4 +94,65 @@ setInterval(oto,5000);
 
 function renk(){
   konusmacilarimiz.style.background = 'rgba(255, 255, 255, 0.25)';
+}
+
+const saatE = document.querySelector('.saat');
+const dakikaE = document.querySelector('.dakika');
+const saniyeE = document.querySelector('.saniye');
+const gunE = document.querySelector('.gun');
+const yeniyil = "15 March 2023";
+function gerisayim() {
+  
+  const yeniyilzaman = new Date(yeniyil);
+  const suan = new Date();
+  var kontrolgun = gunE.textContent;
+  var kontrolsaat = saatE.textContent;
+  var kontroldakika = dakikaE.textContent;
+  var kontrolsaniye = saniyeE.textContent;
+  const kalansaniye = (yeniyilzaman - suan) / 1000;
+  const gun = Math.floor(kalansaniye / 3600 / 24);
+  const saat = Math.floor(kalansaniye / 3600) % 24;
+  const dakika = Math.floor(kalansaniye / 60) % 60;
+  const saniye = Math.floor(kalansaniye) % 60;
+
+    
+ 
+  if (kontrolgun != gun) {
+    gunE.style.color = 'rgba(0, 255, 37)';
+    setTimeout(color2 , 500);
+  }
+  if (kontrolsaat != saat) {
+    saatE.style.color = 'rgba(0, 255, 37)';
+    setTimeout(color3 , 500);
+  }
+  if (kontroldakika != dakika) {
+    dakikaE.style.color = 'rgba(0, 255, 37)';
+    setTimeout(color4 , 500);
+  }
+  if (kontrolsaniye != saniye) {
+    saniyeE.style.color = 'rgba(0, 255, 37)';
+    setTimeout(color5 , 500);
+  }
+
+  gunE.innerHTML = gun;
+  saatE.innerHTML = kontrol(saat);
+  dakikaE.innerHTML = kontrol(dakika);
+  saniyeE.innerHTML = kontrol(saniye);
+}
+setInterval(gerisayim,1000);
+function kontrol(kontrol) {
+  return kontrol < 10 ? `0${kontrol}` : kontrol;
+}
+
+function color2(){
+  gunE.style.color = 'rgba(255, 255, 255,1)';
+}
+function color3(){
+  saatE.style.color = 'rgba(255, 255, 255,1)';
+}
+function color4(){
+  dakikaE.style.color = 'rgba(255, 255, 255,1)';
+}
+function color5(){
+  saniyeE.style.color = 'rgba(255, 255, 255,1)';
 }
